1becaf47dc362d26193a556c86f6f52b8d9767c9
Eduardo <eduardo.benigno15@gmail.com>
2024-05-20 09:41:05 -0300

    feat: finish class 0301 and starting 0302

--- 0202_project-setup/src/test/Utils.test.ts
+++
@@ -1,8 +0,0 @@
-import { toUppercase } from "../app/Utils"
-
-describe('Utils test suite', () => {
-  test('should return uppercase', () => {
-    const result = toUppercase('abc')
-    expect(result).toBe('ABC')
-  })
-})
