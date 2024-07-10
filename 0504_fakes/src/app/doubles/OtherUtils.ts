ea94ea890bc5f767b2892d243b6da4141e0ecabb
Eduardo <eduardo.benigno15@gmail.com>
2024-07-10 08:18:43 -0300

    feat: class 0504

--- 0504_fakes/src/app/doubles/OtherUtils.ts
+++ 0504_fakes/src/app/doubles/OtherUtils.ts
@@ -6,6 +6,17 @@ export type stringInfo = {
   extraInfo: Object | undefined
 }
 
-export function calculateComplexity(stringInfo: stringInfo) {
+type LoggerServiceCallBack = (arg: string) => void;
+
+export function calculateComplexity(stringInfo: stringInfo){
   return Object.keys(stringInfo.extraInfo).length * stringInfo.length
+}
+
+export function toUpperCaseWithCb(arg: string, callBack:LoggerServiceCallBack){
+  if(!arg) {
+      callBack('Invalid argument!');
+      return;
+  }
+  callBack(`called function with ${arg}`)
+  return arg.toUpperCase();
 }
