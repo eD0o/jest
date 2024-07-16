ec827572aba1704601f513f3d52f29032a5e04b4
Eduardo <eduardo.benigno15@gmail.com>
2024-07-16 15:36:19 -0300

    feat: finishing class 0507

--- 0507_mocking-modules/src/test/doubles/OtherUtils.test.ts
+++ 0507_mocking-modules/src/test/doubles/OtherUtils.test.ts
@@ -1,6 +1,6 @@
 import { calculateComplexity, OtherStringUtils, toUpperCaseWithCb } from "../../app/doubles/OtherUtils"
 
-describe('OtherUtils test suite', () => {
+describe.skip('OtherUtils test suite', () => {
 
   describe.only('OtherStringUtils tests with spies', () => {
 
