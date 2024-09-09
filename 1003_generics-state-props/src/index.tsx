e3b781d9086c2e876111f8e9ef8657789c0f42f4
Eduardo <eduardo.benigno15@gmail.com>
2024-09-09 08:21:04 -0300

    feat: finishing classes 1003 and 1004

--- 1003_generics-state-props/src/index.tsx
+++ 1003_generics-state-props/src/index.tsx
@@ -1,12 +1,19 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App2 from './App2';
+import LoginComponent from './LoginComponent';
+import LoginService from './services/LoginService';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const loginService = new LoginService();
+
+const setToken = (token:string) =>{
+  console.log(`received the token ${token}`)
+}
+
 root.render(
-  <App2 />
+  <LoginComponent loginService={loginService} setToken={setToken} />
 );
 
